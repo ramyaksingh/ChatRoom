c28bf7a4755e3841fd03e96eba1ed2afc9c9f462
ramyaksingh <ramyaksingh@yahoo.com>
2017-07-16 15:41:19 -0400

    Server side implementation for multiple clients using threading

--- server_programming.py
+++ server_programming.py
@@ -1,26 +1,45 @@
 import socket
+from threading import *
 
-HOST, PORT = '', 8000
+class AsyncWrite(Thread):
 
-listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-clients = {}
-listen_socket.bind((HOST, PORT))
-listen_socket.listen(10)
-print('Serving HTTP on port %s ...' % PORT)
+    def __init__(self, serverToClientSocket, clientAddress):
+        Thread.__init__(self)
+        self.serverToClientSocket = serverToClientSocket
+        self.clientAddress = clientAddress
+        self.start()
 
-while True:
+    def run(self):
+        self.serverToClientSocket.send("Enter your name: ".encode())
+        name = self.serverToClientSocket.recv(1024).decode()
+        print("New client in room. Say hello to " + name)
 
-    client_connection, client_address = listen_socket.accept()
-    clients[client_address] = client_connection
-    print('New client in room ...' + str(client_address[0]) + ' ' + str(client_address[1]))
+        while True:
+            self.serverToClientSocket.send("Write something: ".encode())
 
-    while(True):
+            str = self.serverToClientSocket.recv(1024).decode()
+            if(str == 'exit'):
+                self.serverToClientSocket.close()
+                break
 
 
-        request = clients[client_address].recv(1024)
+            print(name + str)
 
-        print(request.decode())
 
-        clients[client_address].send("Type something else: ".encode())
 
-clients[client_address].close()
+def Main():
+    HOST, PORT = '', 8888
+
+    listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    listen_socket.bind((HOST, PORT))
+    listen_socket.listen(10)
+    print('Serving HTTP on port %s ...' % PORT)
+
+    while True:
+
+        client_connection, client_address = listen_socket.accept()
+        #print('New client in room ...' + str(client_address[0]) + ' ' + str(client_address[1]))
+        AsyncWrite(client_connection, client_address)
+
+if(__name__ == '__main__'):
+    Main()
